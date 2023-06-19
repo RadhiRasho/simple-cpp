f3b543e32167e2ee8d92e84bf15bdfe83f23de4c
Radhi B. Rasho <Radhi.h@outlook.com>
2023-06-19 14:06:01 +0000

    initial commit

---
+++ games/sum-two-numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool again = true;
+    int x = 0, y = 0;
+
+    do
+    {
+        string choice = "";
+        cout << "Enter Two Numbers To Add Together: ";
+        cin >> x;
+        cin >> y;
+
+        int sum = x + y;
+
+        cout << "The Sum Is: " << sum << endl;
+
+        cout << "Wanna Go Again (Y/N)? " << endl;
+
+        cin >> choice;
+
+        transform(choice.begin(), choice.end(), choice.begin(), ::toupper);
+        if (choice == "N" || choice == "NO") {
+            again = false;
+        }
+    } while (again == true);
+    return 0;
+}
---
+++ myfirstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << sqrt(64) << endl;
+    cout << round(2.4) << endl;
+    cout << log(3) << endl;
+
+    return 0;
+}
